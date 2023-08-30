f856464a681cf83818960a4d985c22d5f2b903a8
ruwen <ruwen.yi@hotmail.com>
2023-08-30 17:16:18 +0800

    feat(addTask):add new task

--- kanban.js
+++ kanban.js
@@ -2,14 +2,22 @@
 document.addEventListener('click', handleClick);
 
 let taskTarget;
-let oldContent = null;
+let oldContent = '';
+let isEditing;
 function handleClick(event) {
+
+    if (event.target.classList.contains('board-body-task-add-btn')){
+        addTask(event.target);
+        return;
+    }
     
     if (event.target.classList.contains('board-body-task')){
-        showBtn(event.target);
-
+        if (isEditing) return;
+                
         if (!oldContent) oldContent = event.target.innerHTML;
         taskTarget = editTask(event.target);
+
+        showBtn(event.target);
         
         return;
     }
@@ -30,20 +38,40 @@ function handleClick(event) {
 
 }
 
+function addTask(target) {
+    let div = document.createElement('div');
+    div.classList.add('board-body-task');
+    div.setAttribute('contenteditable','true');
+
+    let span = document.createElement('span');
+    span.classList.add('board-body-task-content');
+    span.textContent = 'Task';
+    
+    div.append(span);
+    target.before(div);
+}
+
 function editTask(taskTarget) {
+    isEditing = true;
     taskTarget.onblur = taskTarget.focus;
     
     return taskTarget;
 }
 
 function saveEdit(taskTarget) {
+    isEditing = false;
+
     taskTarget.onblur = '';
     taskTarget.blur();
+
+    oldContent = '';
 
     return;
 }
 
 function cancelEdit(taskTarget, oldContent) {
+    isEditing = false;
+    
     taskTarget.onblur = '';
     taskTarget.blur();
 
